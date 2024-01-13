06db44223a301500dba1a2becbdc10548c427e41
ADRIAN STAVNIC <adrian.stavnic@stud.ubbcluj.ro>
2024-01-13 17:10:10 +0200

    Creare pagina Home pentru sortarea vacantelor pe categorii

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ProiectSite.Data;
+using ProiectSite.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ProiectSite.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ProiectSiteContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ProiectSiteContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public List<Vacation> Vacations { get; set; }
 
+        public async Task<IActionResult> OnGetAsync(int? categoryId)
+        {
+            if (categoryId.HasValue)
+            {
+                Vacations = await _context.Vacation
+                    .Where(v => v.CategoryID == categoryId)
+                    .Include(v => v.Category)
+                    .ToListAsync();
+            }
+            else
+            {
+                Vacations = await _context.Vacation
+                    .Include(v => v.Category)
+                    .ToListAsync();
+            }
+
+            return Page();
         }
     }
 }
